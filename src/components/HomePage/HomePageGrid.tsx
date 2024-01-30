7b91da0e849d8e9e8fcae81f501bb3a5c48f28de
dima danilov <dima.danilov.2012@mail.ru>
2024-01-30 15:07:16 +0300

    add: infinite scroll

--- src/components/HomePage/HomePageGrid.tsx
+++ src/components/HomePage/HomePageGrid.tsx
@@ -7,6 +7,26 @@ import { Loader } from "components/common/Loader/Loader";
 export const HomePageGrid = observer(() => {
   const catStore = CatStore;
 
+  const handleScroll = () => {
+    const bottom =
+      Math.ceil(window.innerHeight + window.scrollY) >=
+      document.documentElement.scrollHeight;
+
+    if (bottom) {
+      catStore.loadCats();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, {
+      passive: true,
+    });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   useEffect(() => {
     if (catStore.cats.length === 0) {
       catStore.loadCats();
@@ -14,17 +34,11 @@ export const HomePageGrid = observer(() => {
   }, []);
 
   return (
-    <>
+    <div onScroll={handleScroll}>
       {catStore.isLoading && (
         <Loader className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/3 z-50" />
       )}
       <ImageGrid imageArray={catStore.cats} />
-      <button
-        className="w-full border-2 border-black p-2"
-        onClick={() => catStore.loadCats()}
-      >
-        Load more cats
-      </button>
-    </>
+    </div>
   );
 });
--- src/components/common/ImageGrid/ImageGrid.tsx
+++ src/components/common/ImageGrid/ImageGrid.tsx
@@ -7,7 +7,7 @@ type ImageGrid = {
 export const ImageGrid = ({ imageArray }: ImageGrid) => {
   return (
     <div
-      className={`grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 items-center justify-center`}
+      className={`grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 items-center justify-center pb-40`}
     >
       {imageArray.map((catImage: CatImageModel, index: number) => (
         <ImageCard
