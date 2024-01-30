9cbdb03df8a03a4a6c898c71f43a8ba307d85bee
dima danilov <dima.danilov.2012@mail.ru>
2024-01-30 16:02:03 +0300

    fix: deploy settings + add readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "pages/HomePage";
 import { FavouritesPage } from "pages/FavouritesPage";
 import { Header } from "components/common/Header/Header";
 
-const router = createBrowserRouter([
+const router = createMemoryRouter([
   {
     path: "/",
     element: (
